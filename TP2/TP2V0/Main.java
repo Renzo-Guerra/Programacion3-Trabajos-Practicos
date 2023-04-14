a9afba1ae1277d436ebc51603346dbd84eff18b5
renzo-guerra <renzoguerralatt@gmail.com>
2023-04-14 10:44:39 -0300

    ADD: E1 - Metodo de ordenamiento por seleccion

---
+++ TP2/TP2V0/Main.java
@@ -0,0 +1,55 @@
+package TP2.TP2V0;
+
+public class Main {
+  public static void main(String[] args) {
+    //Ejercicio 1 A
+    
+    int[] arr = {12, 43, 2, 3, 1, 5, 56, 7};
+    System.out.println("Testing del ejercicio 1: ");
+    System.out.println("Arreglo original: ");
+    imprimirArreglo(arr);
+    ordSeleccion(arr);
+    System.out.println("Arreglo ordenado: ");
+    imprimirArreglo(arr);  
+  }
+
+  /**
+   * Dado un array de integers, los ordena de menor a mayor 
+   * utilizando el ordenamiento por seleccion.
+   * 
+   * O(sumatoria(n)) donde n es la cantidad de elementos
+   * @param numeros a ordenar
+   */
+  public static void ordSeleccion(int[] numeros){
+    if(numeros.length < 2) {return;}
+
+    for(int i = 0;i < (numeros.length-1);i++){
+      int menor = i;
+      int j = i+1;
+      while(j < numeros.length){
+        if(numeros[j] < numeros[menor]){
+          menor = j;
+        }
+        j++;
+      }
+      
+      /*
+       * En caso de que se haya encontrado un numero 
+       * menor al del indice actual, se los intercambia
+       */
+      if(menor > i){
+        int aux = numeros[i];
+        numeros[i] = numeros[menor];
+        numeros[menor] = aux;
+      }
+      
+    }
+  }
+
+  public static void imprimirArreglo(int[] arr){
+    for(int i = 0;i<arr.length;i++){
+      System.out.print(arr[i] + " | ");
+    }
+    System.out.println();
+  }
+}
