113039c021f2a25f5b8c78071505e83c9c49480d
renzo-guerra <renzoguerralatt@gmail.com>
2023-05-29 11:35:00 -0300

    ADD: estructura base de BFS

---
+++ TP3/servicios/ServicioBFS.java
@@ -0,0 +1,21 @@
+package TP3.servicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import TP3.grafo.Grafo;
+
+public class ServicioBFS {
+
+	private Grafo<?> grafo;
+	
+	public ServicioBFS(Grafo<?> grafo) {
+		this.grafo = grafo;
+	}
+	
+	public List<Integer> bfsForest() {
+		// Resolver BFS
+		return new ArrayList<>();
+	}
+	
+}
