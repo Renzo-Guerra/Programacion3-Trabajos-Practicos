b9bcfa7682d633c425b906537f55f21cbd2bf0ff
renzo-guerra <renzoguerralatt@gmail.com>
2023-05-31 19:33:25 -0300

    Revert "TESTING: add testing del metodo esCiclico"
    This reverts commit 6d3def05b3f129a81e6bc50d0cf1251d6e15be1d.

--- TP3/Main.java
+++ TP3/Main.java
@@ -1,19 +1,7 @@
 package TP3;
 
-import TP3.grafo.GrafoDirigido;
-
 public class Main {
   public static void main(String[] args) {
-    GrafoDirigido<Integer> grafo = new GrafoDirigido<>();
-    grafo.agregarVertice(1);
-    grafo.agregarVertice(2);
-    grafo.agregarVertice(3);
-
-    grafo.agregarArco(1, 2, 10);
-    grafo.agregarArco(2, 3, 20);
-    // Comentar y descomentar la siguiente linea para cambiar los resultados de la consola.
-    grafo.agregarArco(3, 1, 30);  
-
-    System.out.println("Es ciclico: " + grafo.esCiclico());
+    
   }
 }
